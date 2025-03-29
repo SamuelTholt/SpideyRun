f5a19adc62756d92cfee9495c0e8b96cbea6a5a9
SamuelTholt <148647490+SamuelTholt@users.noreply.github.com>
2025-03-29 18:28:14 +0100

    animacie behu + strafing vlavo/vpravo

--- Assets/Scripts/PohybHraca.cs
+++ Assets/Scripts/PohybHraca.cs
@@ -4,6 +4,10 @@ public class PohybHraca : MonoBehaviour
 {
     public float moveSpeed = 10.0f;
 
+    public Animator playerAnim;
+
+    public bool strafing;
+
     private Vector3 leftPosition = new Vector3(15.12413f, 1.055456f, -18.66f);
     private Vector3 middlePosition = new Vector3(15.12413f, 1.055456f, -13.87f);
     private Vector3 rightPosition = new Vector3(15.12413f, 1.055456f, -9.08f);
@@ -21,24 +25,49 @@ public class PohybHraca : MonoBehaviour
         Pohyb();
 
         transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+
+         if (playerAnim != null && transform.position == targetPosition)
+        {
+            playerAnim.SetTrigger("run");
+        }
     }
 
     private void Pohyb()
     {
+
+        if (playerAnim == null) return;
+
         if ((Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)) && targetPosition != leftPosition)
         {
             if (targetPosition == rightPosition)
+            {
+                playerAnim.SetTrigger("left_strafe");
+                playerAnim.ResetTrigger("run");
                 targetPosition = middlePosition;
+            }
             else
+            {
+                playerAnim.SetTrigger("left_strafe");
+                playerAnim.ResetTrigger("run");
                 targetPosition = leftPosition;
+            }
+
         }
 
         if ((Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)) && targetPosition != rightPosition)
         {
             if (targetPosition == leftPosition)
+            {
+                playerAnim.SetTrigger("right_strafe");
+                playerAnim.ResetTrigger("run");
                 targetPosition = middlePosition;
+            }
             else
+            {
+                playerAnim.SetTrigger("right_strafe");
+                playerAnim.ResetTrigger("run");
                 targetPosition = rightPosition;
+            }
         }
     }
 }
